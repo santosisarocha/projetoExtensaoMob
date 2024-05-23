9041f661298a13af518ffc383f51d30cbf668116
santosisarocha <123705785+santosisarocha@users.noreply.github.com>
2024-05-23 10:01:06 -0300

    first commit

---
+++ Geolocation/routers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import App from './App'; // Importe o componente App
+
+export default function Main() {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+}
